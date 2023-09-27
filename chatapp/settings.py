8cc6a2a474516950ee4462b973c309eaead7b844
Fulati Aizihaer <azhar.pulat@gmail.com>
2023-09-26 23:04:03 -0700

    vercel uploading codes

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vyycvbr^-wutzhz5dn91a(1(9_ns%tn7xe+&173lbl9c(@o%=5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
